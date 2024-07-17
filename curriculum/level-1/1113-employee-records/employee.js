bf0740f80821be3fae162a25f9e96b39fa0d8d70
Gary Combs <gcombs71112@gmail.com>
2024-07-17 18:19:15 -0500

    made some changes

--- curriculum/level-1/1113-employee-records/employee.js
+++ curriculum/level-1/1113-employee-records/employee.js
@@ -1,4 +1,4 @@
-function Employee(name, jobTitle, salary, status) {
+function Employee(name, jobTitle, salary, status = "Full Time") {
   this.name = name;
   this.jobTitle = jobTitle;
   this.salary = salary;
@@ -6,32 +6,34 @@ function Employee(name, jobTitle, salary, status) {
   this.printEmployeeForm = function () {
     console.log(
       "Hello my name is " +
-        this.name +
-        " and I am the " +
-        this.jobTitle +
-        " I currently earn " +
-        this.salary +
-        " and I work " +
-        this.status
+      this.name +
+      " and I am the " +
+      this.jobTitle +
+      " I currently earn " +
+      this.salary +
+      " and I work " +
+      this.status
     );
   };
 }
 
-let employee1 = new Employee("Django", "boss", "2 million/year", "full time");
+let employee1 = new Employee("Django", "boss", "2 million/year");
+
+
+//console.log(employees)
 employee1.printEmployeeForm();
+
 
 let employee2 = new Employee(
   "Prince",
   "The Artist",
   "2 thousand/month",
-  "part time"
 );
+employee2.status = "Part time"
 employee2.printEmployeeForm();
 
-let employee3 = new Employee(
-  "Shrek",
-  "Forest Ogre",
-  "20 dollars/hr",
-  "full time"
-);
+let employee3 = new Employee("Shrek", "Forest Ogre", "20 dollars/hr");
 employee3.printEmployeeForm();
+
+const employees = [employee1, employee2, employee3];
+console.log(employees)
